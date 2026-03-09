09c2a16a1dcb09e198a8e01daa04b4be57a11f64
André Lopes <dedlop@gmail.com>
2026-03-08 23:03:01 -0300

    Organizando os componets em pasta

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
 import './styles/themes.css';
 import './styles/global.css';
 
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+
 export function App() {
 
     return (
         <>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        LOGO
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        MENU
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        FORM
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        FOOTER
-                    </section>
-                </div>
-            </div>
+            <Container>
+                <Heading>LOGO</Heading>
+            </Container>
+
+            <Container>
+                <Heading>MENU</Heading>
+            </Container>
         </>
     )
 }
